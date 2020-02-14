2eb18c2c2582e016b1148afa97dc398ffbf83f95
Jérémy Reynard <jeremy.reynard@cosmotech.com>
2020-02-14 15:57:10 +0100

    Update libraries version Fix compilation errors

--- src/main/java/com/techprimers/graphql/springbootgrapqlexample/model/Book.java
+++ src/main/java/com/techprimers/graphql/springbootgrapqlexample/model/Book.java
@@ -1,18 +1,13 @@
 package com.techprimers.graphql.springbootgrapqlexample.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Setter
 @Getter
-@AllArgsConstructor
-@NoArgsConstructor
 @Table
 @Entity
 public class Book {
@@ -23,4 +18,16 @@ public class Book {
     private String publisher;
     private String[] authors;
     private String publishedDate;
+
+    public Book() {
+    }
+
+    public Book(String isn, String title, String publisher, String[] authors,
+        String publishedDate) {
+        this.isn = isn;
+        this.title = title;
+        this.publisher = publisher;
+        this.authors = authors;
+        this.publishedDate = publishedDate;
+    }
 }
